f1d92e68153de826589850bf50bee8268ba307f8
Daniel Henderson <tindrum@mac.com>
2017-02-27 20:10:11 -0800

    Initial Commit

---
+++ MVC example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVC example
+//
+//  Created by Daniel Henderson on 2/27/17.
+//  Copyright © 2017 Daniel Henderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

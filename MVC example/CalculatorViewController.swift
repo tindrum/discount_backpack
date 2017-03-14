5a9e697ab1dab401453d9790635f2f812fa0bab0
Daniel Henderson <tindrum@mac.com>
2017-03-14 04:09:26 -0700

    calculator is working

--- MVC example/CalculatorViewController.swift
+++ MVC example/CalculatorViewController.swift
@@ -8,29 +8,30 @@
 
 import UIKit
 
+// get currencty symbol from locale 
+// iOS Programming pg 121
+let currentLocale = Locale.current
+let currencySymbol = currentLocale.currencySymbol
+
 class CalculatorViewController: UIViewController, UITextFieldDelegate {
     let calcData:Calculator = Calculator()
     var currentTextField:UITextField?
     
+    
     @IBAction func editChanged(_ sender: UITextField) {
-        currentTextField = sender
         if Float(sender.text!) != nil {
             switch sender {
                 case price:
                     calcData.price = Float(sender.text!)!
                 case dollarsOff:
                     calcData.dollarsOff = Float(sender.text!)!
-                case discount:
-                    calcData.discount = Float(sender.text!)!
-                case otherDiscount:
-                    calcData.otherDiscount = Float(sender.text!)!
-                case tax:
-                    calcData.tax = Float(sender.text!)!
+                case discount, otherDiscount, tax:
+                    calcData.discount = Float(sender.text!)! / 100.0
                 default:
                     print("what text field got clicked?")
             }
-            originalPrice.text = String(calcData.price)
-            discountPrice.text = String(calcData.discountPrice)
+            originalPrice.text = currencySymbol! + dollarFormatter.string(from: NSNumber(value:calcData.price))!
+            discountPrice.text = currencySymbol! + dollarFormatter.string(from: NSNumber(value:calcData.discountPrice))!
         }
     }
     
@@ -55,6 +56,24 @@ class CalculatorViewController: UIViewController, UITextFieldDelegate {
             return false
         } else {
             return true
+        }
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        currentTextField = textField
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        switch textField {
+        case price, dollarsOff:
+            let moneyString:String = convertToMoney(textField.text)
+            textField.text = moneyString
+        case discount, otherDiscount, tax:
+            let percentString:String = convertToPercent(textField.text)
+            textField.text = percentString
+        default:
+            print("what text field got edited?")
         }
     }
     
@@ -95,4 +114,36 @@ class CalculatorViewController: UIViewController, UITextFieldDelegate {
     // Enable unwinding other views
     @IBAction func unwindToCalc(segue:UIStoryboardSegue) {}
     
+    // Number formatters from "Swift Programming: The Big Nerd Ranch Guide, 2nd Ed."
+    // by M. Mathias and J. Gallagher, page 81
+    //
+    // If there is already a decimal in the number,
+    // suppress the addition of another one.
+    let dollarFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 2
+        nf.maximumFractionDigits = 2
+        return nf
+    }()
+    
+    // It is strange that the text must be converted to Float
+    // in order to use the format string.
+    // I guess anything but a number of some sort
+    // can't be searched for a place for the decimal point.
+    func convertToMoney(_ text: String?) -> String {
+        if (text != nil){
+            return String(format: "%1.2f", Float(text!)!)
+        } else {
+            return "0.00"
+        }
+    }
+
+    func convertToPercent(_ text: String?) -> String {
+        if (text != nil) {
+            return String(format: "%2.2f", Float(text!)!)
+        } else {
+            return "00.00"
+        }
+    }
 }
